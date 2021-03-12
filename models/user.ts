86031df70deef4c9e497be4d73c2108a2c2035a0
pozhitok-egor <60615053+pozhitok-egor@users.noreply.github.com>
2021-03-12 23:08:58 +0300

    feat: change image data type

--- models/user.ts
+++ models/user.ts
@@ -5,7 +5,7 @@ const userSchema: mongoose.Schema = new mongoose.Schema(
   {
     username: { type: String, required: true },
     password: { type: String, required: true },
-    image: { type: String, data: Buffer, required: true },
+    image: { data: Buffer, contentType: String, required: true },
     accountUrl: { type: String, required: false }
   },
   {
