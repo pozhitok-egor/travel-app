63d8f289ddf0b2874b8ee628cb9c07bbcfc223eb
Egor Pozhitok <pozhitok.e@gmail.com>
2021-03-13 16:57:31 +0300

    feat: add json response limit

--- app.ts
+++ app.ts
@@ -26,7 +26,7 @@ mongoose
     })
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '2mb'}));
 app.use(express.urlencoded({limit: '3mb', extended: false}));
 app.use(cors())
 
