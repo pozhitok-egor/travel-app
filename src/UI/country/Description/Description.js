c246ca1b9f6f2208b24a94c8e2c0b3511c09f437
Egor Pozhitok <pozhitok.e@gmail.com>
2021-03-15 15:55:11 +0300

    feat: add desctiption

--- src/UI/country/Description/Description.js
+++ src/UI/country/Description/Description.js
@@ -1,12 +1,47 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
-const Description = () => {
+const Description = (props) => {
+  console.log(props);
   return (
-    <div>
-      Description
-    </div>
+    <DescriptionBlock>
+      <Image src={props.country.imageUrl} alt={props.country.name[props.lang]}/>
+      <Text>
+        {props.country.description[props.lang]}
+      </Text>
+    </DescriptionBlock>
   )
 }
 
-export default connect(null, null)(Description);
+const mapStateToProps = (state) => {
+  return {
+    country: state.country,
+    lang: state.language
+  }
+}
+
+const DescriptionBlock = styled.div`
+  display: flex;
+  margin: 10px 0;
+  gap: 10px;
+  @media (max-width: 900px) {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+`;
+
+const Image = styled.div`
+  background: url(${({src}) => src});        background-size: cover;
+  min-width: 300px;
+  height: 200px;
+  border-radius: 25px;
+`;
+
+const Text = styled.p`
+  text-align:left;
+  width: 450px;
+`;
+
+
+export default connect(mapStateToProps, null)(Description);
